7281135589a6b568d2cb738e9e7112d63f77982c
Moh-hamdaoui <hamdaoui.mohammed03@gmail.com>
2025-12-03 15:05:57 +0100

    combien de tests vont se lancer

---
+++ teamtrack-backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth');
+const projectRoutes = require('./routes/projects');
+const taskRoutes = require('./routes/tasks');
+const userRoutes = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
+
+module.exports = app;
