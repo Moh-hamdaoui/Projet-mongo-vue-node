1946bf3b9f805a4bf6f9b5270d61418484332166
Moh-hamdaoui <hamdaoui.mohammed03@gmail.com>
2025-12-03 14:16:12 +0100

    est ce que ca va marcher 6 ?

--- teamtrack-backend/routes/userRoutes.js
+++ teamtrack-backend/routes/userRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-const auth = require('../middleware/authMiddleware')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const auth = require('../middleware/authMiddleware');
 
 router.get('/', auth, async (req, res) => {
   try {
-    const users = await User.find({}, 'name email _id') // récupère quelques champs utiles
-    res.json(users)
+    const users = await User.find({}, 'name email _id'); // récupère quelques champs utiles
+    res.json(users);
   } catch (err) {
-    res.status(500).json({ message: 'Erreur lors de la récupération des utilisateurs', error: err })
+    res.status(500).json({ message: 'Erreur lors de la récupération des utilisateurs', error: err });
   }
 })
 
-module.exports = router
+module.exports = router;
