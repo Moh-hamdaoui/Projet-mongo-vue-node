89ade98236c93ea1e7bac2222d1da46685c046a2
Moh-hamdaoui <hamdaoui.mohammed03@gmail.com>
2025-12-03 14:08:02 +0100

    est ce que ca va marcher 6 ?

--- teamtrack-backend/controllers/taskController.js
+++ teamtrack-backend/controllers/taskController.js
@@ -24,7 +24,9 @@ exports.createTask = async (req, res) => {
 
 exports.getTasksByProject = async (req, res) => {
   try {
-    const tasks = await Task.find({ project: req.params.projectId }).populate('assignedTo', 'name')
+    const tasks = await Task.find({ 
+      project: req.params.projectId 
+    }).populate('assignedTo', 'name')
     res.status(200).json(tasks)
   } catch (error) {
     res.status(500).json({ message: 'Erreur lors de la récupération des tâches', error })
